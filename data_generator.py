c5a019a0a3e9fc8402ba60d14f797e1ae9a54f80
A00431008 <Sudeep.Raj.Badal@smu.ca>
2025-03-29 01:44:26 -0300

    Created data_generator.py

---
+++ data_generator.py
@@ -0,0 +1,7 @@
+# Class to generate training data for the autoencoder
+# Following components to be added
+
+# Method to iteratively work through a corpus of prompts, 
+# calling the model to generate text and activations for each prompt
+
+# Method to save the outputs in a format that can be input to the autoencoder for training
